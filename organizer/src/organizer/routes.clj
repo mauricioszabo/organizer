d74fe27df138b1a7ce9f03bf6748624a8c7b53e3
Maurício Szabo <mauricio.szabo@gmail.com>
2018-02-18 17:19:15 -0300

    Removed old code

--- organizer/src/organizer/routes.clj
+++ organizer/src/organizer/routes.clj
@@ -2,19 +2,6 @@
   (:refer-clojure :exclude [get])
   (:require [compojure.core :as compojure]
             [organizer.controllers.documents :as docs]))
-;
-; (macroexpand-1 '
-;                (defroutes foobar
-;                  (GET "/" []
-;                       "FOO")))
-
-(def foobar (compojure/routes (compojure/GET "/" [] "FOO")))
-;
-; (macroexpand '(GET "/foo" [] "FOO"))
-;
-; (def a (GET "/foo" [] "FOO"))
-;
-; (a {:request-method :get :compojure/path "/foo"})
 
 (defn get [path handler]
   (compojure/GET path req (handler req req)))
